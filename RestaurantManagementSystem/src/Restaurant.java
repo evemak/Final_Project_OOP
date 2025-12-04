63b45c520a0f4dccc970d25dc2c4200f4f0432ae
evemak <136145873+evemak@users.noreply.github.com>
2025-12-04 18:52:15 -0500

    changed classes

--- RestaurantManagementSystem/src/Restaurant.java
+++ RestaurantManagementSystem/src/Restaurant.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Restaurant {
-    
+    String name;
+    Menu menu;
+    ArrayList<String> reviews;
+    Inventory Inventory;
+    public Restaurant(String name, Menu menu, ArrayList<String> reviews){
+        this.name = name;
+        this.menu = menu;
+        this.reviews = reviews;
+    }
 }
