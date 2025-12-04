daf49eef158bfb90297041a2eddfc1ddb62ae476
evemak <136145873+evemak@users.noreply.github.com>
2025-12-04 18:55:52 -0500

    updated restaurant class

--- RestaurantManagementSystem/src/Restaurant.java
+++ RestaurantManagementSystem/src/Restaurant.java
@@ -1,13 +1,51 @@
 import java.util.ArrayList;
 
 public class Restaurant {
-    String name;
-    Menu menu;
-    ArrayList<String> reviews;
-    Inventory Inventory;
-    public Restaurant(String name, Menu menu, ArrayList<String> reviews){
+    private String name;
+    private Menu menu;                     // Has-A Menu
+    private Inventory inventory;           // Has-A Inventory
+    private ArrayList<String> reviews;     // List of customer reviews
+
+    // Constructor
+    public Restaurant(String name, Menu menu, Inventory inventory) {
         this.name = name;
         this.menu = menu;
-        this.reviews = reviews;
+        this.inventory = inventory;
+        this.reviews = new ArrayList<>();
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    // Add a review
+    public void addReview(String review) {
+        reviews.add(review);
+    }
+
+    // Staff operations
+    public void addItemToMenu(MenuItem item) {
+        menu.addItem(item);
+    }
+
+    public void updateInventory(MenuItem item, int quantity) {
+        inventory.updateQuantity(item, quantity);
+    }
+
+    public Bill generateBill(Customer customer) {
+        return new Bill(customer.getOrderItems());
     }
 }
