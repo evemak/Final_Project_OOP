9fcf544147bdaa47d4d1e2820fe1ea8bb7a9754c
evemak <136145873+evemak@users.noreply.github.com>
2025-12-04 18:53:40 -0500

    updated menu class

--- RestaurantManagementSystem/src/Menu.java
+++ RestaurantManagementSystem/src/Menu.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Menu {
-    
+    private ArrayList<MenuItem> items;
+
+    public Menu() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
 }
